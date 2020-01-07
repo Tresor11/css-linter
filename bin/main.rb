4e287b55546ddcc1f474ac2f8c99b808f9f83983
Tresor11 <tresor.moise2001@gmail.com>
2020-01-07 20:24:21 +0200

    added the primary folders

---
+++ bin/main.rb
@@ -0,0 +1,57 @@
+class Parser
+  attr_reader :tags
+
+  def initialize(str)
+    @buffer = StringScanner.new(str)
+    @tags   = []
+    parse
+  end
+
+  def parse
+    until @buffer.eos?
+      skip_spaces
+      parse_element
+    end
+  end
+
+  def parse_element
+    if @buffer.peek(1) == '<'
+      @tags << find_tag
+      last_tag.content = find_content
+    end
+  end
+
+  def skip_spaces
+    @buffer.skip(/\s+/)
+  end
+
+  def find_tag
+    @buffer.getch
+    tag = @buffer.scan(/\w+/)
+    @buffer.getch
+    Tag.new(tag)
+  end
+
+  def find_content
+    tag = last_tag.name
+    content = @buffer.scan_until /<\/#{tag}>/
+    content.sub("</#{tag}>", "")
+  end
+
+  def first_tag
+    @tags.first
+  end
+
+  def last_tag
+    @tags.last
+  end
+end
+
+class Tag
+  attr_reader :name
+  attr_accessor :content
+
+  def initialize(name)
+    @name = name
+  end
+end
