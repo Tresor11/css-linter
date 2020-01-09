090e6b29bee838f297513ace801d0eb894ffd64c
Tresor11 <tresor.moise2001@gmail.com>
2020-01-09 21:29:06 +0200

    corrected the lint errors

--- lib/module.rb
+++ lib/module.rb
@@ -2,24 +2,26 @@
 
 module Bot
   def self.chek1(str)
-    if str.match(/.+\s{1}+{/)
-      puts "There should be a new line after '{' on line #{$i}" unless str.match(/.+\s{1}+$/)
+    if str.match(/.+{/)
+      puts "there should be a new line after '{' on line #{$i}" unless str.match(/.+{$/)
+      puts "there should be a white space before '{' on line #{$i}" unless str.match(/.+\s{1}+{/)
     end
   end
 
   def self.chek2(str)
-    if str.match(/\s+[a-z]+.+\s+[a-z]+./)
-      puts "There should not be any capital letter on line #{$i}" if str.match(/[A-Z]/)
-      puts "the value should be asigned with ':' on line #{$i}" unless str.match(/[a-z]+.+:+\s+[a-z]+.$/)
-      puts "the line shoud end with a ';' on line #{$i}" unless str.match(/[a-z]+.+.+\s+[a-z]+;$/)
-      puts "there hould not be any thing after ';' on line #{$i}" unless str.match(/[a-z]+.+:+\s+[a-z]+;\R/)
+    if str.match(/.+[a-z]+.+.+./)
+      puts "there should not be any capital letter on line #{$i}" if str.match(/[A-Z]/)
+      puts "the line should start with a white space on line #{$i}" unless str.match(/\s{2}+[a-z]+.+.+./)
+      puts "the value should be asigned with ':' on line #{$i}" unless str.match(/.+[a-z]+.+:+.+.+./)
+      puts "the line shoud end with a ';' on line #{$i}" unless str.match(/[a-z]+.+.+\s+.+;$/)
+      puts "there should be a new line after ';' on line #{$i}" unless str.match(/[a-z]+.+:+\s+.+;\R/)
     end
   end
 
   def self.chek3(str)
     if str.match(/}/)
-      puts "There should not be anything before '}' on line #{$i}" unless str.match(/^}/)
-      puts "There should be a new line after '}' on line #{$i}" unless str.match(/^}\R$/)
+      puts "there should not be anything before '}' on line #{$i}" unless str.match(/^}/)
+      puts "there should be a new line after '}' on line #{$i}" unless str.match(/^}\R$/)
     end
   end
 
