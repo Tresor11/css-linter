4341b26947278b39b3f24ce3f796d8f823607e15
Tresor11 <tresor.moise2001@gmail.com>
2020-01-09 14:22:35 +0200

    added the methods for checking the errors in the module

--- lib/module.rb
+++ lib/module.rb
@@ -1,14 +1,23 @@
 module Bot
   def self.chek1(str)
     if str.match(/.+\s{1}+{/)
-      puts "too many white spaces on line #{$i}" if str.count(' ') > 3
+      puts "There should be a new line after '{' on line #{$i}" unless str.match(/.+\s{1}+\R/)
     end
   end
 
   def self.chek2(str)
-    if str.match(/\s+[a-z]+.+\s+[a-z]+;/)
-      puts "sytanct correct at line #{$i}" if str.match(/[a-z]+.+:+\s+[a-z]+;$/)
-      puts "incorect sythanx on line #{$i}" unless str.match(/[a-z]+.+:+\s+[a-z]+;$/)
+    if str.match(/\s+[a-z]+.+\s+[a-z]+./)
+      puts "There should not be any capital letter on line #{$i}" if str.match(/[A-Z]/)
+      puts "the value should be asigned with ':' on line #{$i}" unless str.match(/[a-z]+.+:+\s+[a-z]+.$/)
+      puts "the line shoud end with a ';' on line #{$i}" unless str.match(/[a-z]+.+.+\s+[a-z]+;$/)
+      puts "there hould not be any thing after ';' on line #{$i}" unless str.match(/[a-z]+.+:+\s+[a-z]+;\R/)
+    end
+  end
+
+  def self.chek3(str)
+    if str.match(/}/)
+      puts "There should not be anything before '}' on line #{$i}" unless str.match(/^}/)
+      puts "There should be a new line after '}' on line #{$i}" unless str.match(/^}\R$/)
     end
   end
 
@@ -18,6 +27,8 @@ module Bot
       Bot.chek1(arg)
     when 2
       Bot.chek2(arg)
+    when 3
+      Bot.chek3(arg)
     end
   end
 end
